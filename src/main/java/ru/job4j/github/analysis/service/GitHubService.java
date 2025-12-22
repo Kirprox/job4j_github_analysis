208ca8c16d80bbdb952f8e56813f351e457b215b
kir <kirpro95@yandex.ru>
2025-12-22 22:54:01 +0300

    initial commit

---
+++ src/main/java/ru/job4j/github/analysis/service/GitHubService.java
@@ -0,0 +1,36 @@
+package ru.job4j.github.analysis.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import ru.job4j.github.analysis.model.Commit;
+import ru.job4j.github.analysis.model.Repository;
+
+import java.util.List;
+
+@Service
+public class GitHubService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public List<Repository> fetchRepositories(String username) {
+        String url = "https://api.github.com/users/" + username + "/repos";
+        ResponseEntity<List<Repository>> response = restTemplate.exchange(
+                url, HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<Repository>>() {
+                });
+        return response.getBody();
+    }
+
+    public List<Commit> fetchCommits(String repository) {
+        String url = "https://api.github.com/repos/" + repository + "/commits";
+        ResponseEntity<List<Commit>> response = restTemplate.exchange(
+                url, HttpMethod.GET, null,
+                new ParameterizedTypeReference<List<Commit>>() {
+                });
+        return response.getBody();
+    }
+}
